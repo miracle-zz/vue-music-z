96ebf70a3c35bce6f67fa83ce2eec98c848e52ce
miracleZ <799720991@qq.com>
2020-09-02 15:20:52 +0800

    项目目录创建

---
+++ src/components/content/navHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <a href>
+        <i class="el-icon-headset"></i>网易云音乐
+      </a>
+    </div>
+    <div class="search"></div>
+    <div class="login"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navHeader'
+}
+</script>
+
+<style lang="scss">
+.header {
+  width: 100%;
+  height: 60px;
+  background-color: #c20c0c;
+  line-height: 60px;
+  .logo {
+    margin-left: 20px;
+    a {
+      color: #ffffff;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <nav-header></nav-header>
+  </div>
+</template>
+
+<script>
+import navHeader from 'components/content/navHeader'
+export default {
+  name: 'Home',
+  components: {
+    navHeader
+  }
+}
+</script>
+<style>
+</style>
